7d53582c947adeaf7adfd66618b3ebbeecabe4ac
fbcsantiago <fernanda.carvalho1985@gmail.com>
2026-01-07 15:07:43 -0300

    criando ambientes de testes

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId:'pthzhp',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+     return require('./cypress/plugins/index.js')(on, config)
     },
+    baseUrl: 'http://lojaebac.ebaconline.art.br/',
   },
 });
